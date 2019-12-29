22a12f4378c0c51f7d6738c914f7c36e63c745ed
Joaquín Velázquez Camacho <joaquinvelazquezcamacho@gmail.com>
2019-12-29 20:26:24 -0300

    gitignore

---
+++ client/src/types.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} DateTime
+ * @property {number} year
+ * @property {number} month
+ * @property {number} day
+ * @property {number} hour
+ * @property {number} minute
+ */
+
+/**
+ * @typedef {Object} PluginProxy
+ * @property {string} id
+ * @property {DateTime} installedDate
+ * @property {boolean} installed
+ * @property {boolean} active
+ * @property {string[]} registry
+ */
